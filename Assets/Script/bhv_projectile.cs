9682bf9d9be765dbe71fce39d31b7d09a064f726
Vallenteer <kristiantokelvin@yahoo.co.id>
2016-06-21 13:43:53 +0700

    rubah proyektil
    destroy proyektil kalau kelamaan

--- Assets/Script/bhv_projectile.cs
+++ Assets/Script/bhv_projectile.cs
@@ -4,9 +4,18 @@ using System.Collections;
 public class bhv_projectile : MonoBehaviour {
 
 	[SerializeField] int speed = 10;
+	public float lifetime; //bisa dihapus, untuk menghilangkan object
 
 	void Update () {
+		lifetime -= Time.deltaTime;
+
+		if (lifetime < 0) {
+			Destroy (gameObject);
+		}
+
 		this.transform.Translate (new Vector3 (speed, 0, 0) * Time.deltaTime );
 	}
 		
 }
+
+
