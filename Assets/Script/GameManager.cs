b200f75e562544ea32e99d373f6f85ee5ead7e7d
Vallenteer <kristiantokelvin@yahoo.co.id>
2016-07-11 00:47:54 +0700

    camera ubah ke 16:9
    BG uda jalan

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Set camera aspect ratio
+		Camera.main.aspect = 16f/9f;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
