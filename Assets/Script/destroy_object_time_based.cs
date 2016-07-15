b0abe419a2ce74e5f289387346d486b88fb2522b
Vallenteer <kristiantokelvin@yahoo.co.id>
2016-07-15 20:40:24 +0700

    SFX DIpasang ke proyektil
    ketika ditembak keluar suara
    
    dan bkin prefab suara hit dan teriak ouch

---
+++ Assets/Script/destroy_object_time_based.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroy_object_time_based : MonoBehaviour {
+
+	[SerializeField] float lifetime; //bisa dihapus, untuk menghilangkan object
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		lifetime -= Time.deltaTime;
+
+		if (lifetime < 0) {
+			Destroy (gameObject);
+		}
+
+	}
+}
