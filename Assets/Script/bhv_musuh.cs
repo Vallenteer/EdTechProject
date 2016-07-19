646ec9d2be1b42c3a7d2b4276afc1cc7998df913
Vallenteer <kristiantokelvin@yahoo.co.id>
2016-07-19 23:44:53 +0700

    musuh 3 prefab

--- Assets/Script/bhv_musuh.cs
+++ Assets/Script/bhv_musuh.cs
@@ -10,8 +10,6 @@ public class bhv_musuh : MonoBehaviour {
 	mng_soalGenerator soalManager;
 	mng_playerStat statManager;
 
-	//BGM
-	[SerializeField] GameObject BGM;
 
 	[Header("Properties")]
 	[SerializeField] float speed = 5;
@@ -24,9 +22,7 @@ public class bhv_musuh : MonoBehaviour {
 		if (manager == null) { //Init Ref GameObject Manager
 			manager = GameObject.Find ("GameManager").gameObject;
 		}
-		if (!GameObject.FindGameObjectWithTag ("BGM")) {
-			DontDestroyOnLoad (Instantiate (BGM));
-		}
+
 		soalManager = manager.GetComponent<mng_soalGenerator> ();
 		statManager = manager.GetComponent<mng_playerStat> ();
 		carriedAnswer = soalManager.generateJawaban ();
