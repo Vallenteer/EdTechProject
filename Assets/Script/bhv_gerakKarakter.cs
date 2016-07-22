b7c16472c7a7905bad27740e23ef56648d1a73a7
Vallenteer <kristiantokelvin@yahoo.co.id>
2016-07-22 20:39:41 +0700

    android lancar sudah dapat dipakai dan digunakan

--- Assets/Script/bhv_gerakKarakter.cs
+++ Assets/Script/bhv_gerakKarakter.cs
@@ -23,8 +23,16 @@ public class bhv_gerakKarakter : MonoBehaviour {
 		mousePos = new Vector2 (Camera.main.ScreenToWorldPoint (Input.mousePosition).x, Camera.main.ScreenToWorldPoint (Input.mousePosition).y);
 		this.transform.position = (Vector3) mousePos;
 		if (Input.touchCount > 0) {
-			this.transform.position = (Vector3)Input.GetTouch(0).position;
+			// The screen has been touched so store the touch
+			Touch touch = Input.GetTouch(0);
+
+			if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved) {
+				// If the finger is on the screen, move the object smoothly to the touch position
+				Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 0));                
+				transform.position = Vector3.Lerp(transform.position, touchPosition, Time.deltaTime);
+			}
 		}
+
 		if (Input.GetMouseButtonUp(0) && !cooldownState) {
 			Tembak ();
 		}
